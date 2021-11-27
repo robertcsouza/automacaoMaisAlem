823d36d13561510770bf38ca7729f22817d883c9
robertcsouza <robertogeek18@gmail.com>
2021-11-27 01:50:55 -0400

    att model pedido

--- controllers/PedidoController.js
+++ controllers/PedidoController.js
@@ -1,6 +1,6 @@
 "use strict";Object.defineProperty(exports, "__esModule", {value: true}); function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-var _pedido = require('../models/pedido'); var _pedido2 = _interopRequireDefault(_pedido);
+var _pedido = require('../models/Pedido'); var _pedido2 = _interopRequireDefault(_pedido);
 
 
 class PedidoController {
